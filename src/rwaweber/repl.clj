125df393f7a23ad20991db8681ce5119f46d2510
rwaweber <rwaweber@chump.info>
2024-07-14 20:01:25 -0400

    Improve logging, add nrepl-port file support

--- src/rwaweber/repl.clj
+++ src/rwaweber/repl.clj
@@ -1,17 +1,48 @@
 (ns rwaweber.repl
-  (:require [clojure.string :as string]
-            [rebel-readline.clojure.main :as rebel]
+  (:import [java.net Socket])
+  (:require [rebel-readline.clojure.main :as rebel]
+            [clojure.tools.logging :as log]
             [nrepl.server :refer [start-server stop-server]]))
 
-(def repl-port 12345)
+(defn high-port []
+  (+ 62000 (rand-int 3535)))
 
-(defonce myserver
-  (start-server
-   :port repl-port
-   :bind "127.0.0.1"))
+(defn is-file?
+  [filename]
+  (try (slurp filename)
+       true
+       (catch java.io.FileNotFoundException _
+         false)))
+
+(defn find-unused-addr
+  "will attempt to find an unused port
+
+  will check if .nrepl-port is present and return that"
+  ([repl-port]
+   (let [host "127.0.0.1"]
+     (try (Socket. host repl-port)
+          (log/infof
+           "another service already listening on %s:%d, trying another" host repl-port)
+          (find-unused-addr (high-port))
+          (catch java.net.ConnectException _
+            {:bind host
+             :port repl-port}))))
+  ([]
+   (if (is-file? ".nrepl-port")
+     (let [port (read-string (slurp ".nrepl-port"))]
+       (log/infof "found .nrepl-port file, suggesting port %d", port)
+       (find-unused-addr port))
+     (let [port (high-port)]
+       ;; write out port to file
+       (log/infof "no .nrepl-port file found, using %d", port)
+       (spit ".nrepl-port" port)
+       (find-unused-addr port)))))
 
 (defn -main
-  [& args]
-  (println "starting rwaweber.repl on port " repl-port)
-  (rebel/-main)
-  (stop-server myserver))
+  [& _]
+  (let [{:keys [bind port]} (find-unused-addr)
+        repl-server (start-server :bind bind
+                                  :port port)]
+    (log/infof "starting rwaweber.repl on %s:%d" bind port)
+    (rebel/-main)
+    (stop-server repl-server)))
